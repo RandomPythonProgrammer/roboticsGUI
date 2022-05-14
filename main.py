21b43c13b2037e071511d694aa8a8cafab31e29e
RandomPythonProgrammer <74526339+RandomPythonProgrammer@users.noreply.github.com>
2022-05-13 19:08:55 -0500

    Working on a new update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
 
 import pyglet
+from pyglet.window import key
 from enum import Enum
 import os
 import json
@@ -10,7 +11,7 @@ class ActionType(Enum):
     VOID = -1
     ROTATION = 0
     MOVEMENT = 1
-    FUNCTION = 2
+    SLEEP = 2
 
 
 class Direction(Enum):
@@ -37,6 +38,8 @@ class Movement:
             return f"move({self.amount}, Direction.{self.direction.name});"
         elif self.action is ActionType.ROTATION:
             return f"turn({self.amount}, Direction.{self.direction.name});"
+        elif self.action is ActionType.SLEEP:
+            return f"delay({self.amount});"
         elif self.action is ActionType.VOID:
             return None
 
@@ -63,7 +66,7 @@ class Application(pyglet.window.Window):
         self.dragging = False
         self.drag_direction = None
         self.setup = False
-        self.held_keys = pyglet.window.key.KeyStateHandler()
+        self.held_keys = key.KeyStateHandler()
         with open(os.path.join(os.path.dirname(__file__), 'config.json'), 'r') as config:
             self.settings = json.load(config)
         self.set_caption("RoboticsGUI")
@@ -75,7 +78,7 @@ class Application(pyglet.window.Window):
         self.pixel_per_meter = self.height / (self.tileSize * 6)
         self.backgroundBatch = pyglet.graphics.Batch()
         self.foregroundBatch = pyglet.graphics.Batch()
-        path = os.path.join(os.path.dirname(__file__), 'field.png')
+        path = os.path.join(os.path.dirname(__file__), 'resources/field.png')
         self.background = pyglet.sprite.Sprite(pyglet.image.load(path), 0, 0, batch=self.backgroundBatch)
         self.field_size = round(self.tileSize * self.pixel_per_meter * 6)
         self.background.scale_x = self.field_size / self.background.width
@@ -84,7 +87,7 @@ class Application(pyglet.window.Window):
         # initialize objects
         width = self.settings['robot_width'] * self.pixel_per_meter
         length = self.settings['robot_length'] * self.pixel_per_meter
-        path = os.path.join(os.path.dirname(__file__), "robot.png")
+        path = os.path.join(os.path.dirname(__file__), "resources/robot.png")
         image = pyglet.image.load(path)
         image.anchor_x, image.anchor_y = round(image.width / 2), round(image.height / 2)
         self.robot = pyglet.sprite.Sprite(image, self.field_size / 2, self.field_size / 2, batch=self.foregroundBatch)
@@ -103,7 +106,7 @@ class Application(pyglet.window.Window):
         self.console_box.x, self.console_box.y = self.field_size, 0
 
     def on_update(self, dt: float):
-        if self.held_keys[pyglet.window.key.LSHIFT] or self.held_keys[pyglet.window.key.RSHIFT]:
+        if self.held_keys[key.LSHIFT] or self.held_keys[key.RSHIFT]:
             return
 
         angle = self.robot.rotation
@@ -124,40 +127,40 @@ class Application(pyglet.window.Window):
         direction = None
         amount = 0
 
-        if self.held_keys[pyglet.window.key.Q]:
+        if self.held_keys[key.Q]:
             self.robot.rotation -= rotation
             action_type = ActionType.ROTATION
             direction = Direction.COUNTERCLOCKWISE
-            amount = rotation
+            amount = rotation * math.pi / 180
 
-        elif self.held_keys[pyglet.window.key.E]:
+        elif self.held_keys[key.E]:
             self.robot.rotation += rotation
             action_type = ActionType.ROTATION
             direction = Direction.CLOCKWISE
-            amount = rotation
+            amount = rotation * math.pi / 180
 
-        elif self.held_keys[pyglet.window.key.W]:
+        elif self.held_keys[key.W]:
             self.robot.x += distance * x_mult
             self.robot.y += distance * y_mult
             action_type = ActionType.MOVEMENT
             direction = Direction.FORWARD
             amount = distance / self.pixel_per_meter
 
-        elif self.held_keys[pyglet.window.key.S]:
+        elif self.held_keys[key.S]:
             self.robot.x -= distance * x_mult
             self.robot.y -= distance * y_mult
             action_type = ActionType.MOVEMENT
             direction = Direction.BACKWARD
             amount = distance / self.pixel_per_meter
 
-        elif self.held_keys[pyglet.window.key.A]:
+        elif self.held_keys[key.A]:
             self.robot.x -= distance * p_x_mult
             self.robot.y -= distance * p_y_mult
             action_type = ActionType.MOVEMENT
             direction = Direction.LEFTWARD
             amount = distance / self.pixel_per_meter
 
-        elif self.held_keys[pyglet.window.key.D]:
+        elif self.held_keys[key.D]:
             self.robot.x += distance * p_x_mult
             self.robot.y += distance * p_y_mult
             action_type = ActionType.MOVEMENT
@@ -166,7 +169,6 @@ class Application(pyglet.window.Window):
 
         if amount > 0:
             self.add_movement(amount, action_type, direction, (position, angle))
-            self.update_console()
 
     def add_movement(self, amount: float, action_type: ActionType, direction: Direction, state: tuple):
         if self.setup and amount > 0:
@@ -179,8 +181,9 @@ class Application(pyglet.window.Window):
                 if (previous.action is action_type and same_direction) or (are_opposite and same_group):
                     movement = self.movements.pop(-1) + movement
 
-            if movement.amount > 0:
+            if movement.amount > 0 or movement.action:
                 self.movements.append(movement)
+                self.update_console()
 
     def on_render(self, dt: float):
         self.clear()
@@ -199,58 +202,64 @@ class Application(pyglet.window.Window):
                                      self.console_box.height - self.field_size / 4)
 
     def on_key_release(self, symbol, modifiers):
-        if symbol == pyglet.window.key.ENTER:
-            self.setup = True
-            self.robot.opacity = 255
         self.held_keys[symbol] = False
 
-        if modifiers & pyglet.window.key.MOD_SHIFT:
-            position = self.robot.position
-            angle = self.robot.rotation
+        position = self.robot.position
+        angle = self.robot.rotation
 
+        if modifiers & key.MOD_SHIFT:
             radians = self.robot.rotation * math.pi / 180
-            if symbol is pyglet.window.key.Q:
+            if symbol is key.Q:
                 new_angle = round((radians - (math.pi / 4)) / (math.pi / 4)) * (math.pi / 4) * 180 / math.pi
                 self.add_movement(
                     abs(angle - new_angle) * math.pi / 180,
                     ActionType.ROTATION, Direction.COUNTERCLOCKWISE, (position, angle)
                 )
-                self.update_console()
                 self.robot.rotation = new_angle
-            elif symbol is pyglet.window.key.E:
+
+            elif symbol is key.E:
                 new_angle = round((radians + (math.pi / 4)) / (math.pi / 4)) * (math.pi / 4) * 180 / math.pi
                 self.add_movement(
                     abs(angle - new_angle) * math.pi / 180,
                     ActionType.ROTATION, Direction.CLOCKWISE, (position, angle)
                 )
-                self.update_console()
                 self.robot.rotation = new_angle
 
-        if symbol is pyglet.window.key.P and modifiers & pyglet.window.key.MOD_ACCEL:
-            for line in self.get_code():
-                print(line)
-            print("--------------------------------------")
+            elif symbol is key.R:
+                self.add_movement(1000, ActionType.SLEEP, Direction.VOID, (position, angle))
 
-        if symbol is pyglet.window.key.Z and modifiers & pyglet.window.key.MOD_ACCEL:
-            if len(self.movements) > 0:
-                movement = self.movements.pop(-1)
-                self.robot.position, self.robot.rotation = movement.state
-                self.update_console()
+        else:
+            if symbol is key.P and modifiers & key.MOD_ACCEL:
+                for line in self.get_code():
+                    print(line)
+                print("--------------------------------------")
 
-        if symbol is pyglet.window.key.C and modifiers & pyglet.window.key.MOD_ACCEL:
-            if len(self.movements) > 0:
-                self.robot.position, self.robot.rotation = self.movements[0].state
-                self.robot.x = self.robot.y = self.field_size / 2
-                self.movements.clear()
-                self.setup = False
-                self.robot.opacity = 200
-                self.update_console()
+            elif symbol is key.Z and modifiers & key.MOD_ACCEL:
+                if len(self.movements) > 0:
+                    movement = self.movements.pop(-1)
+                    self.robot.position, self.robot.rotation = movement.state
+                    self.update_console()
 
-        if symbol is pyglet.window.key.SPACE:
-            self.movements.append(
-                Movement(ActionType.VOID, Direction.VOID, 0, (self.robot.position, self.robot.rotation))
-            )
-            self.update_console()
+            elif symbol is key.C and modifiers & key.MOD_ACCEL:
+                if len(self.movements) > 0:
+                    self.robot.position, self.robot.rotation = self.movements[0].state
+                    self.robot.x = self.robot.y = self.field_size / 2
+                    self.movements.clear()
+                    self.setup = False
+                    self.robot.opacity = 200
+                    self.update_console()
+
+            elif symbol is key.SPACE:
+                self.movements.append(
+                    0, ActionType.VOID, Direction.VOID, (self.robot.position, self.robot.rotation)
+                )
+
+            elif symbol is key.ENTER:
+                self.setup = True
+                self.robot.opacity = 255
+
+            elif symbol is key.R:
+                self.add_movement(100, ActionType.SLEEP, Direction.VOID, (position, angle))
 
     def update_console(self):
         lines = self.get_code()
